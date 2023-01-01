f72b81fcc14a5492641b358641db9f8c51dfe315
Yang Wang (IGBiz) <yvv@meta.com>
2022-12-31 16:50:29 -0800

    drop .docusaurus/* from git

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/yvv/dev/gnavvy.me/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/yvv/dev/gnavvy.me/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/yvv/dev/gnavvy.me/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/yvv/dev/gnavvy.me/src/css/custom.css'),
-];
